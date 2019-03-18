1ab9165ff34bbd234f4418bc332b8c9b62a0d3b3
Dimabest92 <dimabest92@gmail.com>
2019-03-18 15:10:36 +0100

    Switching between conversations added
    Now messages depends on Conversation

--- src/App.js
+++ src/App.js
@@ -11,7 +11,60 @@ import LoginPage from "./components/LoginPage.jsx";
 import MainPage from "./components/MainPage";
 
 
-
+const loadConversations = ()=>{
+    return [
+      {
+        id: 345,
+        messages: [
+                {id: 0, message: 'Cześć!', id_sender: 154},
+                {id: 1, message: 'Hej', id_sender: 463},
+                {id: 2, message: 'Co u Ciebie?', id_sender: 154},
+                {id: 3, message: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis id orci in ligula feugiat condimentum id nec nibh. Curabitur vehicula pretium tortor quis aliquam. Etiam sed tellus pharetra, mattis mauris et, vestibulum erat. Curabitur euismod, tellus sed iaculis egestas, quam erat vestibulum turpis, laoreet egestas magna enim non turpis. Proin sapien lectus, facilisis in urna vel, ultricies imperdiet ligula. Suspendisse potenti. Vivamus feugiat risus a nisi varius, in condimentum erat hendrerit. Quisque in ante sollicitudin eros ultricies posuere vel a eros. Praesent id lorem eu orci molestie varius. Sed quis semper ante. Ut iaculis non massa a mollis. Aliquam egestas eros enim, vitae pretium felis euismod a. Duis congue a sapien at pharetra. Maecenas efficitur in enim fringilla porta. Morbi sagittis quam eget purus iaculis condimentum. Ut ac sodales felis.', id_sender: 154},
+                {id: 4, message: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis id orci in ligula feugiat condimentum id nec nibh. Curabitur vehicula pretium tortor quis aliquam. Etiam sed tellus pharetra, mattis mauris et, vestibulum erat. Curabitur euismod, tellus sed iaculis egestas, quam erat vestibulum turpis, laoreet egestas magna enim non turpis. Proin sapien lectus, facilisis in urna vel, ultricies imperdiet ligula. Suspendisse potenti. Vivamus feugiat risus a nisi varius, in condimentum erat hendrerit. Quisque in ante sollicitudin eros ultricies posuere vel a eros. Praesent id lorem eu orci molestie varius. Sed quis semper ante. Ut iaculis non massa a mollis. Aliquam egestas eros enim, vitae pretium felis euismod a. Duis congue a sapien at pharetra. Maecenas efficitur in enim fringilla porta. Morbi sagittis quam eget purus iaculis condimentum. Ut ac sodales felis.', id_sender: 154},
+            ]
+      },
+      {
+        id: 243,
+        messages: [
+            { id: 0, message: 'Cześć!', id_sender: 154 },
+            { id: 1, message: 'Pa', id_sender: 463 },
+            { id: 2, message: 'Co u Ciebie?', id_sender: 154 },
+            { id: 3, message: 'BlaBlaBla', id_sender: 154 },
+            { id: 4, message: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis id orci in ligula feugiat condimentum id nec nibh. Curabitur vehicula pretium tortor quis aliquam. Etiam sed tellus pharetra, mattis mauris et, vestibulum erat. Curabitur euismod, tellus sed iaculis egestas, quam erat vestibulum turpis, laoreet egestas magna enim non turpis. Proin sapien lectus, facilisis in urna vel, ultricies imperdiet ligula. Suspendisse potenti. Vivamus feugiat risus a nisi varius, in condimentum erat hendrerit. Quisque in ante sollicitudin eros ultricies posuere vel a eros. Praesent id lorem eu orci molestie varius. Sed quis semper ante. Ut iaculis non massa a mollis. Aliquam egestas eros enim, vitae pretium felis euismod a. Duis congue a sapien at pharetra. Maecenas efficitur in enim fringilla porta. Morbi sagittis quam eget purus iaculis condimentum. Ut ac sodales felis.', id_sender: 154 },
+        ]
+      },
+      {
+        id: 834,
+        messages: [
+            { id: 0, message: 'Cześć!', id_sender: 154 },
+            { id: 1, message: 'No Hej', id_sender: 463 },
+            { id: 2, message: 'Co u Ciebie?', id_sender: 154 },
+            { id: 3, message: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis id orci in ligula feugiat condimentum id nec nibh. Curabitur vehicula pretium tortor quis aliquam. Etiam sed tellus pharetra, mattis mauris et, vestibulum erat. Curabitur euismod, tellus sed iaculis egestas, quam erat vestibulum turpis, laoreet egestas magna enim non turpis. Proin sapien lectus, facilisis in urna vel, ultricies imperdiet ligula. Suspendisse potenti. Vivamus feugiat risus a nisi varius, in condimentum erat hendrerit. Quisque in ante sollicitudin eros ultricies posuere vel a eros. Praesent id lorem eu orci molestie varius. Sed quis semper ante. Ut iaculis non massa a mollis. Aliquam egestas eros enim, vitae pretium felis euismod a. Duis congue a sapien at pharetra. Maecenas efficitur in enim fringilla porta. Morbi sagittis quam eget purus iaculis condimentum. Ut ac sodales felis.', id_sender: 154 },
+            { id: 4, message: 'Lorem ipsum dolor sit amet', id_sender: 154 },
+        ]
+      },
+      {
+        id: 153,
+        messages: [
+            { id: 0, message: 'Cześć!', id_sender: 154 },
+            { id: 1, message: 'Hej', id_sender: 463 },
+            { id: 2, message: 'Co u Ciebie?', id_sender: 154 },
+            { id: 3, message: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis id orci in ligula feugiat condimentum id nec nibh. Curabitur vehicula pretium tortor quis aliquam. Etiam sed tellus pharetra, mattis mauris et, vestibulum erat. Curabitur euismod, tellus sed iaculis egestas, quam erat vestibulum turpis, laoreet egestas magna enim non turpis. Proin sapien lectus, facilisis in urna vel, ultricies imperdiet ligula. Suspendisse potenti. Vivamus feugiat risus a nisi varius, in condimentum erat hendrerit. Quisque in ante sollicitudin eros ultricies posuere vel a eros. Praesent id lorem eu orci molestie varius. Sed quis semper ante. Ut iaculis non massa a mollis. Aliquam egestas eros enim, vitae pretium felis euismod a. Duis congue a sapien at pharetra. Maecenas efficitur in enim fringilla porta. Morbi sagittis quam eget purus iaculis condimentum. Ut ac sodales felis.', id_sender: 154 },
+            { id: 4, message: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis id orci in ligula feugiat condimentum id nec nibh. Curabitur vehicula pretium tortor quis aliquam. Etiam sed tellus pharetra, mattis mauris et, vestibulum erat. Curabitur euismod, tellus sed iaculis egestas, quam erat vestibulum turpis, laoreet egestas magna enim non turpis. Proin sapien lectus, facilisis in urna vel, ultricies imperdiet ligula. Suspendisse potenti. Vivamus feugiat risus a nisi varius, in condimentum erat hendrerit. Quisque in ante sollicitudin eros ultricies posuere vel a eros. Praesent id lorem eu orci molestie varius. Sed quis semper ante. Ut iaculis non massa a mollis. Aliquam egestas eros enim, vitae pretium felis euismod a. Duis congue a sapien at pharetra. Maecenas efficitur in enim fringilla porta. Morbi sagittis quam eget purus iaculis condimentum. Ut ac sodales felis.', id_sender: 154 },
+        ]
+      },
+      {
+        id: 152,
+        messages: [
+            { id: 0, message: 'Cześć!', id_sender: 154 },
+            { id: 1, message: 'Hej', id_sender: 463 },
+            { id: 2, message: 'Co u Ciebie?', id_sender: 154 },
+            { id: 3, message: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis id orci in ligula feugiat condimentum id nec nibh. Curabitur vehicula pretium tortor quis aliquam. Etiam sed tellus pharetra, mattis mauris et, vestibulum erat. Curabitur euismod, tellus sed iaculis egestas, quam erat vestibulum turpis, laoreet egestas magna enim non turpis. Proin sapien lectus, facilisis in urna vel, ultricies imperdiet ligula. Suspendisse potenti. Vivamus feugiat risus a nisi varius, in condimentum erat hendrerit. Quisque in ante sollicitudin eros ultricies posuere vel a eros. Praesent id lorem eu orci molestie varius. Sed quis semper ante. Ut iaculis non massa a mollis. Aliquam egestas eros enim, vitae pretium felis euismod a. Duis congue a sapien at pharetra. Maecenas efficitur in enim fringilla porta. Morbi sagittis quam eget purus iaculis condimentum. Ut ac sodales felis.', id_sender: 154 },
+            { id: 4, message: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis id orci in ligula feugiat condimentum id nec nibh. Curabitur vehicula pretium tortor quis aliquam. Etiam sed tellus pharetra, mattis mauris et, vestibulum erat. Curabitur euismod, tellus sed iaculis egestas, quam erat vestibulum turpis, laoreet egestas magna enim non turpis. Proin sapien lectus, facilisis in urna vel, ultricies imperdiet ligula. Suspendisse potenti. Vivamus feugiat risus a nisi varius, in condimentum erat hendrerit. Quisque in ante sollicitudin eros ultricies posuere vel a eros. Praesent id lorem eu orci molestie varius. Sed quis semper ante. Ut iaculis non massa a mollis. Aliquam egestas eros enim, vitae pretium felis euismod a. Duis congue a sapien at pharetra. Maecenas efficitur in enim fringilla porta. Morbi sagittis quam eget purus iaculis condimentum. Ut ac sodales felis.', id_sender: 154 },
+        ]
+      }
+    ];     
+};
 
 
 class App extends Component {
@@ -19,16 +72,24 @@ class App extends Component {
         super(props);
         this.state = {
             isSignIn: true,
+            conversations: loadConversations()
         }
         //methods binding
         this.changeAutorizationStatus = this.changeAutorizationStatus.bind(this);
+        this.getMessagesForConversation = this.getMessagesForConversation.bind(this);
     }
 
    changeAutorizationStatus = () => {
        this.setState((prevState) => {
            return { isSignIn: !prevState.isSignIn };
        })
-       console.log(this.state.isSignIn);
+    };
+
+    getMessagesForConversation = (id) => {
+        for (let i = 0; i < this.state.conversations.length; i++) {
+            if (this.state.conversations[i].id == id)
+                return this.state.conversations[i].messages;
+        }
     };
 
   render() {
@@ -39,7 +100,9 @@ class App extends Component {
                     autorizationStatus={this.state.isSignIn}                     
                     />
                 <Switch className="bg-dark" style={{height: '100%'}}>
-                    <Route path="/" component={MainPage} exact/>                    
+                    <Route path="/" 
+                        render={()=><MainPage onConvarsationChange={this.getMessagesForConversation}
+                        actualConversationID={this.state.conversations[0].id}/>} exact/>                    
                     <Route path="/logout" 
                         render={() => <LogoutPage changeAutorizationStatus={this.changeAutorizationStatus}/>} exact />
                     <Route path="/login" render={() => <LoginPage/>} exact />
@@ -50,8 +113,5 @@ class App extends Component {
     );
   }
 }
-
-//<Route path="/login" component={LoginPage} exact />
-
 
 export default App;
