8c65f2a597b863ead08a254cfd34430f6d6f69b7
Dimabest92 <dimabest92@gmail.com>
2019-05-07 19:47:43 +0200

    Corrected previous commit

--- src/components/LogoutPage.jsx
+++ src/components/LogoutPage.jsx
@@ -6,6 +6,9 @@ class LogoutPage extends Component {
     constructor(props){
         super(props);
         this.props.changeAutorizationStatus();
+        this.props.changeToken('');
+        this.props.changeUserId('');
+        this.props.changeTokenExpiration('');
     }
 
     render() {
